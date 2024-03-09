61e4861794c2b2ee390715b229421123cbe6f345
zhangxy567 <zhangxy567@gmail.com>
2024-03-09 16:58:19 +0800

    1

--- 24_2_16/demo1/main.c
+++ 24_2_16/demo1/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int b;
 int main()
 {
-	int a;
-	printf("%d\n",a);
-	printf("%d",b);
+	int a=10;
+	int arr[a]={0};
+	int i=0;
+	for(i=0;i<=9;i++)
+	{
+		printf("%d",arr[i]); 
+	}
 	return 0;
 }
